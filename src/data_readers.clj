6aecd69036dc016b98876e48c82439fb9e1ecd6b
Claus Spacil <claus@spacil.com>
2021-03-17 09:16:12 +0100

    Update data_readers.clj

--- src/data_readers.clj
+++ src/data_readers.clj
@@ -1,4 +1,4 @@
-{d/dbg   debux.core/dbg-tag
- d/dbgn  debux.core/dbgn-tag
- d/clog  debux.cs.core/clog-tag
- d/clogn debux.cs.core/clogn-tag}
+{d/dbg   debux-stubs.core/dbg-tag
+ d/dbgn  debux-stubs.core/dbgn-tag
+ d/clog  debux-stubs.cs.core/clog-tag
+ d/clogn debux-stubs.cs.core/clogn-tag}
